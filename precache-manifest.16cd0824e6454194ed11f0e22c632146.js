f8049e1eaeb54d6d228cdf162b9a7e5795fa8e2e
Lucas Teles De Lima <ltm02@ukdgkwxhbwj1gp.bskyb.com>
2020-02-12 17:19:13 +0000

    Updates

---
+++ precache-manifest.16cd0824e6454194ed11f0e22c632146.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d9e3b64964bdf9434609d3270027afe",
+    "url": "/NowTV-React-Interview/index.html"
+  },
+  {
+    "revision": "ed8dd1009767acdbaed4",
+    "url": "/NowTV-React-Interview/static/js/2.ecd78ab5.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/NowTV-React-Interview/static/js/2.ecd78ab5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5ccb759b0a7b7b165d5",
+    "url": "/NowTV-React-Interview/static/js/main.47e9bb35.chunk.js"
+  },
+  {
+    "revision": "410954d44c4cf89abdf3",
+    "url": "/NowTV-React-Interview/static/js/runtime-main.998e6ca5.js"
+  },
+  {
+    "revision": "f98a80b2947ff65d0fce44211e07d2b2",
+    "url": "/NowTV-React-Interview/static/media/lucas.f98a80b2.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/NowTV-React-Interview/precache-manifest.16cd0824e6454194ed11f0e22c632146.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/NowTV-React-Interview/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
